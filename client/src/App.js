fa2fe74367dcb7755a28d9699793387dfbb822b7
ChalogeKya <zhabibek4u@gmail.com>
2023-04-29 22:39:25 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-
 function App() {
   return (
-    <div className="App ml-4 mt-4">
-        <h1>Jai ho sadfbaba ki </h1>
-    </div>
+    <>
+    hi and hello
+    </>
   );
 }
 
